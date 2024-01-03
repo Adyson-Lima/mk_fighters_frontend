0f73590e852813c5474220a99537dd480f804547
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-03 11:50:50 -0300

    8° commit - tdd tela fighters: Existe botão editar em Fighters? RED

--- src/tests/Fighters.test.js
+++ src/tests/Fighters.test.js
@@ -24,4 +24,8 @@ describe("Testes da tela Fighters", () => {
     expect(screen.getByTestId('mytable')).toBeInTheDocument();
   });
 
+  it ('Existe botão editar em Fighters?', () => {
+    expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
+  });
+
 });
