d5d19cfd06d5e37c57da1872b46e209333002a2a
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-03 11:41:44 -0300

    5° commit - tdd tela Fighters: Existe link novo em Fighters? GREEN

--- src/pages/Fighters/index.js
+++ src/pages/Fighters/index.js
@@ -10,6 +10,9 @@ export default function Fighters(){
       </div>
       <div className="card-body">
 
+      <Link data-testid="mylink" className="btn btn-success" 
+      style={{marginBottom: '10px'}} to="/newupdate/0">Novo</Link>
+      
       </div>
     </div>
   );
