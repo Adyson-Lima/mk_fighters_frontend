c7b7d0260d70a16f27470d5fba700286bce42dbe
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-03 11:59:41 -0300

    10° commit - tdd tela Fighters: Existe botão excluir em Fighters? RED

--- src/tests/Fighters.test.js
+++ src/tests/Fighters.test.js
@@ -28,4 +28,8 @@ describe("Testes da tela Fighters", () => {
     expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
   });
 
+  it ('Existe botão excluir em Fighters?', () => {
+    expect(screen.getByTestId('mybtn2')).toBeInTheDocument();
+  });
+
 });
